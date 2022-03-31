4de707fce4bef50a8279df327c99f8b542948d3b
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-03-31 12:42:11 +0100

    ...

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ for (let i = 0; i < tab.length; i++) {
 
   tab[i].className = tab[i].className.replace("active", "");
 }
+
+let modeButton = document.getElementById("mode-button");
+let body = document.getElementById("body");
+let navbar = document.getElementById("navbar");
+
+function mode() {
+  if (modeButton.classList.contains("text-white")) {
+    modeButton.classList.replace("text-white", "text-black");
+    body.classList.replace("bg-navbar", "bg-white");
+    navbar.classList.replace("bg-navbar", "bg-white");
+  } else if (modeButton.classList.contains("text-black")) {
+    modeButton.classList.replace("text-black", "text-white");
+    body.classList.replace("bg-white", "bg-navbar");
+    navbar.classList.replace("bg-white", "bg-navbar");
+  }
+}
